4c980f4850004236cc5a0025bcfe9c3959bf6039
Tkachenko Roman <githubpo4ta@gmail.com>
2023-11-17 20:53:40 +0200

    created my_profile page

---
+++ app/src/main/java/com/example/android_level_1/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.android_level_1
+
+import android.content.res.Configuration
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.android_level_1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            Toast.makeText(this, "NOW ORIENTATION PORTRAIT", Toast.LENGTH_SHORT).show()
+        }
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Toast.makeText(this, "NOW ORIENTATION LANDSCAPE", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
